0c7275345368aa0f36ac4ccc720cff084e3989b4
Davide Depau <davide@depau.eu>
2019-02-13 21:43:49 +0100

    Add forgotten getNotificationBuilder

---
+++ kotlet-android/src/main/java/eu/depau/kotlet/android/extensions/ui/context/ContextGetNotificationBuilder.kt
@@ -0,0 +1,13 @@
+package eu.depau.kotlet.android.extensions.ui.context
+
+import android.app.Notification
+import android.content.Context
+import android.os.Build
+
+@Suppress("DEPRECATION")
+fun Context.getNotificationBuilder(channel: String): Notification.Builder {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        Notification.Builder(this, channel)
+    else
+        Notification.Builder(this)
+}
